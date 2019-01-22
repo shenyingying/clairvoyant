e889ee810df126652df8552848b4d593ddeb160a
shenyingying <shenyingying25@gmail.com>
2019-01-22 16:22:42 +0800

    test

--- 22.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 19-1-22 下午3:56
-# @Author  : shenyingying
-# @Email   : shen222ying@163.com
-# @File    : 22.py
-# @Software: PyCharm
--- test.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 19-1-22 下午3:49
-# @Author  : shenyingying
-# @Email   : shen222ying@163.com
-# @File    : test.py
-# @Software: PyCharm
-print("hello")
